c7546eb693aa03a0da558d7b5b5fc494a94d1ae2
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-10-20 18:51:13 +0200

    Improve build_all script

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -14,18 +14,43 @@ if [ "$overwrite" != "--overwrite" ] && ls "$outdir"/*"$version"* 2>/dev/null; t
 fi
 
 ./scripts/build_lvgl_micropython.sh esp32 prod fri3d-2024
+result=$?
+if [ $result -ne 0 ]; then
+	echo "build_lvgl_micropython.sh esp32 prod fri3d-2024 got error: $result"
+	exit 1
+fi
 cp "$buildfile" "$outdir"/MicroPythonOS_fri3d-2024_prod_"$version".bin
 cp "$updatefile" "$updatesdir"/MicroPythonOS_fri3d-2024_prod_"$version".ota
 
 ./scripts/build_lvgl_micropython.sh esp32 dev fri3d-2024
+result=$?
+if [ $result -ne 0 ]; then
+	echo "build_lvgl_micropython.sh esp32 dev fri3d-2024 got error: $result"
+	exit 1
+fi
 cp "$buildfile" "$outdir"/MicroPythonOS_fri3d-2024_dev_"$version".bin
 
 ./scripts/build_lvgl_micropython.sh esp32 prod waveshare-esp32-s3-touch-lcd-2
+result=$?
+if [ $result -ne 0 ]; then
+	echo "build_lvgl_micropython.sh esp32 prod waveshare-esp32-s3-touch-lcd-2 got error: $result"
+	exit 1
+fi
 cp "$buildfile" "$outdir"/MicroPythonOS_waveshare-esp32-s3-touch-lcd-2_prod_"$version".bin
 cp "$updatefile" "$updatesdir"/MicroPythonOS_waveshare-esp32-s3-touch-lcd-2_prod_"$version".ota
 
 ./scripts/build_lvgl_micropython.sh esp32 dev waveshare-esp32-s3-touch-lcd-2
+result=$?
+if [ $result -ne 0 ]; then
+	echo "build_lvgl_micropython.sh esp32 dev waveshare-esp32-s3-touch-lcd-2 got error: $result"
+	exit 1
+fi
 cp "$buildfile" "$outdir"/MicroPythonOS_waveshare-esp32-s3-touch-lcd-2_dev_"$version".bin
 
 ./scripts/build_lvgl_micropython.sh unix dev
 cp "$builddir"/lvgl_micropy_unix "$outdir"/MicroPythonOS_amd64_linux_"$version".elf
+result=$?
+if [ $result -ne 0 ]; then
+	echo "build_lvgl_micropython.sh esp32 unix dev got error: $result"
+	exit 1
+fi
