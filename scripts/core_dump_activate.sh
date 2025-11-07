af254abbff89c9287f1e7c69186fd90f3fa7f62e
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-11-07 10:51:10 +0100

    core_dump_activate.py: improve help comments

--- scripts/core_dump_activate.sh
+++ scripts/core_dump_activate.sh
@@ -1,9 +1,18 @@
 # you want this not stripped:
-# file ~/projects/MicroPythonOS/lvgl_micropython/build/lvgl_micropy_unix
-# ~/projects/MicroPythonOS/lvgl_micropython/build/lvgl_micropy_unix: ELF 64-bit LSB pie executable, x86-64, version 1 (SYSV), dynamically linked, interpreter /lib64/ld-linux-x86-64.so.2, BuildID[sha1]=f5c2fd31fd06ac76f9ba4eb031b383dfbc3a1b3c, for GNU/Linux 3.2.0, with debug_info, not stripped
+# file lvgl_micropython/build/lvgl_micropy_unix
+# ~/lvgl_micropython/build/lvgl_micropy_unix: ELF 64-bit LSB pie executable, x86-64, version 1 (SYSV), dynamically linked, interpreter /lib64/ld-linux-x86-64.so.2, BuildID[sha1]=f5c2fd31fd06ac76f9ba4eb031b383dfbc3a1b3c, for GNU/Linux 3.2.0, with debug_info, not stripped
 # 
-# To get that, run it with STRIP= and maybe also that LV_CFLAGS:
+# To get that, compile it with STRIP= and maybe also that LV_CFLAGS - build_mpos.sh will do this by default already:
 # python3 make.py "$target" LV_CFLAGS="-g -O0 -ggdb" STRIP=  DISPLAY=sdl_display INDEV=sdl_pointer INDEV=sdl_keyboard "$manifest"
 
 ulimit -c unlimited
 sudo sysctl -w kernel.core_pattern=/tmp/core.%p
+
+# Now run it as usual (with run_desktop.sh)
+# And when it crashes, the core will be dumped in /tmp/core.XXXXX
+
+# Then analyse it with:
+# gdb lvgl_micropython/build/lvgl_micropy_unix /tmp/core.185491
+
+# For example, to see the stack trace of the error:
+# (gdb) info stack
