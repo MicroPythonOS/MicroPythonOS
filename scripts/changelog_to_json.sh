0a182ba98adaed66910cc0798bac63d85f64a03f
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-11-08 10:09:46 +0100

    Update scripts/changelog_to_json.sh

--- scripts/changelog_to_json.sh
+++ scripts/changelog_to_json.sh
@@ -1,2 +1,4 @@
 sed -i "s/\"/'/g" CHANGELOG.md # change double to single quotes
-cat CHANGELOG.md | tr -d "\n" | sed 's/- /\\n- /g'
+#cat CHANGELOG.md | tr -d "\n" | sed 's/- /\\n- /g'
+sed ':a;N;$!ba;s/\n/\\n/g' CHANGELOG.md
+
