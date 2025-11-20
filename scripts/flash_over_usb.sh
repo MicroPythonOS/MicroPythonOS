7a13f63cef43cc42f12dafe95ced8fd6a733f2f1
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-11-20 22:19:39 +0100

    Fix flash_over_usb.sh

--- scripts/flash_over_usb.sh
+++ scripts/flash_over_usb.sh
@@ -3,7 +3,7 @@ mydir=$(dirname "$mydir")
 fwfile="$0"
 # This would break the --erase-all
 #if [ -z "$fwfile" ]; then
-	#fwfile="$mydir/../lvgl_micropython/build/lvgl_micropy_ESP32_GENERIC_S3-SPIRAM_OCT-16.bin"
+fwfile="$mydir/../lvgl_micropython/build/lvgl_micropy_ESP32_GENERIC_S3-SPIRAM_OCT-16.bin"
 #fi
 ls -al $fwfile
 echo "Add --erase-all if needed"
