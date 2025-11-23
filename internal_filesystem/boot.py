04a27b069cd8b9ba1016574be5b773c5b3e49505
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-11-23 05:26:17 +0100

    Comments

--- internal_filesystem/boot.py
+++ internal_filesystem/boot.py
@@ -1,4 +1,4 @@
-# This file is the only one that can't be overridden (without rebuilding) for development because it's not in lib/, so keep it minimal.
+# This file is the only one that can't be overridden for development (without rebuilding) because it's not in lib/, so keep it minimal.
 
 # Make sure the storage partition's lib/ is first in the path, so whatever is placed there overrides frozen libraries.
 # This allows any build to be used for development as well, just by overriding the libraries in lib/
