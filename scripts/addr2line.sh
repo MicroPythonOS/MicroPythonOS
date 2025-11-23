230de1215bd1d28625782489e29c124ebf066793
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-11-23 21:43:39 +0100

    Update addr2line.sh

--- scripts/addr2line.sh
+++ scripts/addr2line.sh
@@ -1,1 +1,1 @@
-~/.espressif/tools/xtensa-esp-elf/esp-14.2.0_20241119/xtensa-esp-elf/bin/xtensa-esp-elf-addr2line -e lvgl_micropython/lib/micropython/ports/esp32/build-ESP32_GENERIC_S3-SPIRAM_OCT/micropython.elf 
+~/.espressif/tools/xtensa-esp-elf/esp-14.2.0_20241119/xtensa-esp-elf/bin/xtensa-esp-elf-addr2line -e lvgl_micropython/lib/micropython/ports/esp32/build-ESP32_GENERIC_S3-SPIRAM_OCT/micropython.elf
