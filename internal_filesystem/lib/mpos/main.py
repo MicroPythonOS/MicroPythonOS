129775f0391bef9c07365d4c687440066e2f72af
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-11-23 07:14:59 +0100

    Fix board identification

--- internal_filesystem/lib/mpos/main.py
+++ internal_filesystem/lib/mpos/main.py
@@ -15,11 +15,11 @@ if sys.platform == "linux" or sys.platform == "darwin": # linux and macOS
 elif sys.platform == "esp32":
     from machine import Pin, I2C
     i2c0 = I2C(0, sda=Pin(48), scl=Pin(47))
-    if i2c0.scan() == [21, 107]: # touch screen and IMU
+    if {0x15, 0x6B} <= set(i2c0.scan()): # touch screen and IMU (at least, possibly more)
         board = "waveshare_esp32_s3_touch_lcd_2"
     else:
         i2c0 = I2C(0, sda=Pin(9), scl=Pin(18))
-        if i2c0.scan() == [107]: # only IMU
+        if {0x6B} <= set(i2c0.scan()): # IMU (plus possibly the Communicator's LANA TNY at 0x38)
             board = "fri3d_2024"
         else:
             print("Unable to identify board, defaulting...")
