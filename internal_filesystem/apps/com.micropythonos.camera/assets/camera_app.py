5d61da1f6d46d26d8efc1cd595423c3edceab6ac
Thomas Farstrike <thomasverstreken@protonmail.com>
2026-01-13 10:43:37 +0100

    Camera app: simplify imports

--- internal_filesystem/apps/com.micropythonos.camera/assets/camera_app.py
+++ internal_filesystem/apps/com.micropythonos.camera/assets/camera_app.py
@@ -2,6 +2,6 @@
 Camera app wrapper that imports from the mpos.ui.camera_activity module.
 """
 
-from mpos.ui.camera_activity import CameraActivity
+from mpos import CameraActivity
 
 __all__ = ['CameraActivity']
