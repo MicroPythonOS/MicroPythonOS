bfe3d821c440baac32be0daf89d560a7ceb9bb78
Pavel Machek <pavel@ucw.cz>
2026-02-13 19:54:06 +0100

    calendar: more metadata tweaks

--- internal_filesystem/apps/cz.ucw.pavel.calendar/assets/main.py
+++ internal_filesystem/apps/cz.ucw.pavel.calendar/assets/main.py
@@ -255,7 +255,7 @@ class OrgCalendarStore:
 # Calendar Activity
 # ------------------------------------------------------------
 
-class Hello(Activity):
+class Main(Activity):
 
     def __init__(self):
         super().__init__()
