be89ba0dced37845cdde6bc5d82280bdfb9217d3
Thomas Farstrike <thomasverstreken@protonmail.com>
2026-02-18 18:11:45 +0100

    mklittlefs: use internal_filesystem by default

--- scripts/mklittlefs.sh
+++ scripts/mklittlefs.sh
@@ -6,6 +6,8 @@ mydir=$(dirname "$mydir")
 #size=0x200000 # 2MB
 #~/sources/mklittlefs/mklittlefs -c "$mydir"/../internal_filesystem/ -s "$size" internal_filesystem.bin
 
-size=0x520000
-~/sources/mklittlefs/mklittlefs -c "$mydir"/../../../internalsd_zips_removed_gb_romart -s "$size" internalsd_zips_removed_gb_romart.bin
+#size=0x520000
+#~/sources/mklittlefs/mklittlefs -c "$mydir"/../../../internalsd_zips_removed_gb_romart -s "$size" internalsd_zips_removed_gb_romart.bin
 
+size=0x520000
+~/sources/mklittlefs/mklittlefs -c "$mydir"/../internal_filesystem/ -s "$size" internal_filesystem.bin
