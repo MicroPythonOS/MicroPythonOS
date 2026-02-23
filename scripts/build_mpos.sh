51056eaad97f5241db02995d9a7c093c33208518
Thomas Farstrike <thomasverstreken@protonmail.com>
2026-02-23 23:34:08 +0100

    Fix typo

--- scripts/build_mpos.sh
+++ scripts/build_mpos.sh
@@ -102,7 +102,7 @@ if [ "$target" == "esp32" -o "$target" == "esp32s3" -o "$target" == "esp32s3_qem
 		fi
 	fi
 	manifest=$(readlink -f "$codebasedir"/manifests/manifest.py)
-	#frozenmanifest="FROZEN_MANIFEST=$manifest" # Comment this out if you want to make a build without any frozen files, just an empty MicroPython + whatever files you have on the internal storage
+	frozenmanifest="FROZEN_MANIFEST=$manifest" # Comment this out if you want to make a build without any frozen files, just an empty MicroPython + whatever files you have on the internal storage
 	echo "Note that you can also prevent the builtin filesystem from being mounted by umounting it and creating a builtin/ folder."
 	pushd "$codebasedir"/lvgl_micropython/
 	rm -rf lib/micropython/ports/esp32/build-$BOARD-$BOARD_VARIANT
