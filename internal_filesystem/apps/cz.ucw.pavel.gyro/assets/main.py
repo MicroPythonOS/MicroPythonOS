c07121a940e1cf849df19ccb47643e64ec4de6c9
Pavel Machek <pavel@ucw.cz>
2026-03-03 23:01:26 +0100

    gyro: add reset and calibration support, introduce vectors, display help image

--- internal_filesystem/apps/cz.ucw.pavel.gyro/assets/main.py
+++ internal_filesystem/apps/cz.ucw.pavel.gyro/assets/main.py
@@ -1,6 +1,5 @@
 """
-xxx
-
+Test/visualization of gyroscope / accelerometer
 
 """
 
@@ -32,37 +31,118 @@ def to_rad(deg):
 def to_deg(rad):
     return rad * 180.0 / math.pi
 
+class Vec3:
+    def __init__(self):
+        pass
+
+    def init3(self, x, y, z):
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
+        return self
+
+    def init_v(self, v):
+        self.x = v[0]
+        self.y = v[1]
+        self.z = v[2]
+        return self
+
+    def __add__(self, other):
+        return vec3(
+            self.x + other.x,
+            self.y + other.y,
+            self.z + other.z
+        )
+
+    def __sub__(self, other):
+        return vec3(
+            self.x - other.x,
+            self.y - other.y,
+            self.z - other.z
+        )
+
+    def __mul__(self, scalar):
+        return vec3(
+            self.x * scalar,
+            self.y * scalar,
+            self.z * scalar
+        )
+
+    def __truediv__(self, scalar):
+        return vec3(
+            self.x / scalar,
+            self.y / scalar,
+            self.z / scalar
+        )
+
+    __rmul__ = __mul__
+
+    def __repr__(self):
+        return f"X {self.x:.2f} Y {self.y:.2f} Z {self.z:.2f}"
+
+def vec3(x, y, z): return Vec3().init3(x, y, z)
+def vec0(): return Vec3().init3(0, 0, 0)
+
 # -----------------------------
 # Calibration + heading
 # -----------------------------
 
 class Gyro:
-    def reset(self):
-        pass
-
-class TiltGyro(Gyro):
     def __init__(self):
         super().__init__()
-        self.heading = 0
+        self.rot = vec0()
         self.last = time.time()
-        self.smooth = 0.0
+        self.last_reset = self.last
+        self.smooth = vec0()
+        self.calibration = vec0()
 
-    def heading_tilted(self):
+    def reset(self):
+        now = time.time()
+        self.calibration = self.rot / (now - self.last_reset)
+        print("Reset... ", self.calibration)
+        self.last_reset = now
+        self.rot = vec0()
+
+    def update(self):
         """
         Returns heading 0..360
 
         iio is in rads/second
         """
         t = time.time()
-        # pp: val[1] seems to be rotation "away" and "towards" the user, like pitch in plane ... or maybe roll?
-        # val[2] sseems to be rotation -- as useful for compass on table
-        v = self.val[2]
-        coef = 1.0
+        # pp: gyr[1] seems to be rotation "away" and "towards" the user, like pitch in plane ... or maybe roll?
+        # gyr[2] sseems to be rotation -- as useful for compass on table
+        v = self.gyr
+        coef = 1
         self.smooth = self.smooth * (1-coef) + v * coef
-        self.heading -= self.smooth * (t - self.last)
+        self.rot -= self.smooth * (t - self.last)
         self.last = t
-        self.angvel = -self.smooth
-        return self.heading
+
+    def angle(self):
+        now = time.time()
+        return self.rot - (now - self.last_reset) * self.calibration
+
+    def angvel(self):
+        return vec0()-self.smooth
+
+class UGyro(Gyro):
+    def __init__(self):
+        super().__init__()
+
+        self.accel = SensorManager.get_default_sensor(SensorManager.TYPE_ACCELEROMETER)
+        self.magn = SensorManager.get_default_sensor(SensorManager.TYPE_MAGNETIC_FIELD)
+        self.gyro = SensorManager.get_default_sensor(SensorManager.TYPE_GYROSCOPE)
+
+        self.gyr = None
+
+    def update(self):
+        acc = SensorManager.read_sensor_once(self.accel)
+        sc = 1/9.81
+        acc = vec3( -acc[0] * sc, acc[1] * sc, acc[2] * sc )
+        self.acc = acc
+
+        self.gyr = Vec3().init_v(SensorManager.read_sensor_once(self.gyro))
+        super().update()
 
 # -----------------------------
 # Canvas (LVGL)
@@ -370,36 +450,29 @@ class PagedCanvas(Activity):
 # App logic
 # ----------------------------
 
-class UGyro(TiltGyro):
-    def __init__(self):
-        super().__init__()
-
-        self.accel = SensorManager.get_default_sensor(SensorManager.TYPE_ACCELEROMETER)
-        self.magn = SensorManager.get_default_sensor(SensorManager.TYPE_MAGNETIC_FIELD)
-        self.gyro = SensorManager.get_default_sensor(SensorManager.TYPE_GYROSCOPE)
-
-        self.val = None
-        self.vfirst = None
-
-        self.heading = 45
-
-    def update(self):
-        acc = SensorManager.read_sensor_once(self.accel)
-        sc = 1/9.81
-        acc = ( -acc[0] * sc, acc[1] * sc, acc[2] * sc )
-        self.acc = acc
-
-        self.val = SensorManager.read_sensor_once(self.gyro)
-
 class Main(PagedCanvas):
+    ASSET_PATH = "M:apps/cz.ucw.pavel.gyro/res/gyro-help.png"
+
     def __init__(self):
         super().__init__()
 
         self.cal = UGyro()
-
-        self.heading = 0.0
-
         self.Ypos = 40
+
+        img = lv.image(lv.layer_top())
+        img.set_src(f"{self.ASSET_PATH}")
+        self.help_img = img        
+        self.hide_img()
+
+    def hide_img(self):
+        self.help_img.add_flag(lv.obj.FLAG.HIDDEN)
+
+    def draw_img(self):
+        img = self.help_img
+        img.remove_flag(lv.obj.FLAG.HIDDEN)
+        img.set_pos(60, 18)
+        #img.set_size(640, 640)
+        img.set_rotation(0)
 
     def draw(self):
         pass
@@ -414,48 +487,71 @@ class Main(PagedCanvas):
         st = 20
 
         self.cal.update()
-        if self.cal.val is None:
+        if self.cal.gyr is None:
             self.c.text(0, y, f"No compass data")
             y += st
             return
 
-        self.heading = self.cal.heading_tilted()
-
+        if self.page == 2:
+            self.draw_img()
+            return
+        self.hide_img()
+        
         if self.page == 0:
             self.draw_top(self.cal.acc)
-        if self.page == 1:
+        elif self.page == 1:
             self.draw_values()
-        elif self.page == 2:
+        elif self.page == 3:
             self.c.text(0, y, f"Resetting calibration")
             self.page = 0
             self.cal.reset()
 
     def build_buttons(self):
-        self.template_buttons(["Pg0", "Pg1", "Pg2"])
+        self.template_buttons(["Graph", "Values", "Help", "Reset"])
 
     def draw_values(self):
-        x, y, z = self.cal.acc[0], self.cal.acc[1], self.cal.acc[2]
+        x, y, z = self.cal.acc.x, self.cal.acc.y, self.cal.acc.z
         total = math.sqrt(x*x+y*y+z*z)
         s = ""
         if x > .6:
-            s += ", left"
+            s += " left"
         if x < -.6:
-            s += ", right"
+            s += " right"
         if y > .6:
-            s += ", up"
+            s += " up"
         if y < -.6:
-            s += ", down"
+            s += " down"
         if z > .6:
-            s += ", below"
+            s += " below"
         if z < -.6:
-            s += ", above"
+            s += " above"
+
+        t = ""
+        lim = 25
+        angvel = self.cal.angvel()
+        if angvel.z > lim:
+            # top part moves to the right
+            t += " yaw+"
+        if angvel.z < -lim:
+            t += " yaw-"
+        if angvel.x > lim:
+            # top part goes up
+            t += " pitch+"
+        if angvel.x < -lim:
+            t += " pitch-"
+        if angvel.y > lim:
+            # right part goes down
+            t += " roll+"
+        if angvel.y < -lim:
+            t += " roll-"
             
         self.c.text(0, 7, f"""
-^ Up -> Right
-|| Acc
-X {self.cal.acc[0]:.2f} Y {self.cal.acc[1]:.2f} Z {self.cal.acc[2]:.2f}
-{total*100:.2f}% {s}
-X {self.cal.val[0]:.2f} Y {self.cal.val[1]:.2f} Z {self.cal.val[2]:.2f}
+^ Up            -> Right
+||             Acc
+{self.cal.acc}
+Earth is{s}, {total*100:.0f}%
+{self.cal.gyr}
+Rotation is{t}
 """)
 
     def _px_per_deg(self):
@@ -469,11 +565,11 @@ X {self.cal.val[0]:.2f} Y {self.cal.val[1]:.2f} Z {self.cal.val[2]:.2f}
     # ---- TOP VIEW ----
 
     def draw_top(self, acc):
-        heading=self.heading
-        heading2=self.cal.angvel
+        heading=self.cal.angle().z
+        heading2=self.cal.angvel().z
         vmin=0
         vmax=20
-        v=self.cal.val
+        v=self.cal.gyr
 
         cx = self.c.W // 2
         cy = self.c.H // 2
@@ -519,7 +615,7 @@ X {self.cal.val[0]:.2f} Y {self.cal.val[1]:.2f} Z {self.cal.val[2]:.2f}
         self.c.line(poly[4], poly[5], poly[0], poly[1])
 
     def _draw_accel(self, acc):
-        ax, ay, az = acc
+        ax, ay, az = acc.x, acc.y, acc.z
         cx = self.c.W / 2.0
         cy = self.c.H / 2.0
 
