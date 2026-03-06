2e8a47d34fc0cc776455c19b26a51fdd28a891d8
Thomas Farstrike <thomasverstreken@protonmail.com>
2026-03-06 16:42:48 +0100

    update make_image.sh

--- scripts/make_image.sh
+++ scripts/make_image.sh
@@ -12,6 +12,7 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
+rm "$mydir"/../internal_filesystem.bin
 "$mydir"/../scripts/mklittlefs.sh
 
 prboom="~/projects/MicroPythonOS/claude/retro-go/prboom-go/build/prboom-go.bin"
@@ -27,7 +28,8 @@ outdir=lvgl_micropython/lib/micropython/ports/esp32/build-ESP32_GENERIC_S3-SPIRA
 rm image_esp32s3.bin
 #~/.espressif/python_env/*/bin/python -m esptool --chip esp32s3 merge_bin --fill-flash-size=4MB --output image_esp32s3.bin 0x0 "$outdir"/bootloader/bootloader.bin 0x8000 partitions_for_qemu.bin 0x20000 "$outdir"/micropython.bin # 0x820000 "$launcher" 0x930000 "$core" 0xa00000 "$prboom" # 0xae0000 "$mydir"/../internalsd_zips_removed_gb_romart.bin $@
 #~/.espressif/python_env/*/bin/python -m esptool --chip esp32s3 merge_bin --fill-flash-size=8MB --output image_esp32s3.bin 0x0 "$outdir"/bootloader/bootloader.bin 0x8000 partitions_for_qemu.bin 0x20000 "$outdir"/micropython.bin 0x3A0000 "$mydir"/../internal_filesystem.bin $@
-~/.espressif/python_env/*/bin/python -m esptool --chip esp32s3 merge_bin --fill-flash-size=16MB --output image_esp32s3.bin 0x0 "$outdir"/bootloader/bootloader.bin 0x8000 partitions_with_retro-go_16mb.bin 0x20000 "$outdir"/micropython.bin 0x820000 "$launcher" 0x930000 "$core" 0xa00000 "$prboom" 0xae0000 "$mydir"/../internal_filesystem.bin $@
+#~/.espressif/python_env/*/bin/python -m esptool --chip esp32s3 merge_bin --fill-flash-size=16MB --output image_esp32s3.bin 0x0 "$outdir"/bootloader/bootloader.bin 0x8000 partitions_with_retro-go_16mb.bin 0x20000 "$outdir"/micropython.bin 0x820000 "$launcher" 0x930000 "$core" 0xa00000 "$prboom" 0xae0000 "$mydir"/../internal_filesystem.bin $@
+~/.espressif/python_env/*/bin/python -m esptool --chip esp32s3 merge_bin --fill-flash-size=16MB --output image_esp32s3.bin 0x0 "$outdir"/bootloader/bootloader.bin 0x8000 partitions_with_retro-go_16mb.bin 0x20000 "$outdir"/micropython.bin 0xae0000 "$mydir"/../internal_filesystem.bin $@
 
 # Building an image based on an Arduino IDE build also works, although I only tried arduino-esp32 v2.x and not to v3.x
 #outdir=~/.cache/arduino/sketches/4012C161135E5B60169BDFEA7F67E0C6
