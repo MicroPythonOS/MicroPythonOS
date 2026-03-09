5d4744e623879ac99a28053a04e4ee4f243d292b
Thomas Farstrike <thomasverstreken@protonmail.com>
2026-03-09 12:50:06 +0100

    Simplify mpong build

---
+++ c_mpos/mpong/build.sh
@@ -0,0 +1,13 @@
+#. /home/user/projects/MicroPythonOS/claude/.espressif/python_env/idf5.4_py3.11_env/bin/activate
+
+mydir=$(readlink -f "$0")
+mydir=$(dirname "$mydir")
+
+cd "$mydir"
+
+rm -rf build
+rm *.mpy
+
+PATH=~/.espressif/tools/xtensa-esp-elf/esp-14.2.0_20241119/xtensa-esp-elf/bin/:$PATH make </dev/null
+
+mv mpong*.mpy ../../internal_filesystem/apps/com.micropythonos.mpong/assets/
