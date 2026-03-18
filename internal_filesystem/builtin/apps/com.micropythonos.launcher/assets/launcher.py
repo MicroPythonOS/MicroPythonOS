a13af57273610cf035f4187243e084c760201ac1
Thomas Farstrike <thomasverstreken@protonmail.com>
2026-03-18 13:05:19 +0100

    Launcher: ignore launchers and MPOS settings (except wifi)

--- internal_filesystem/builtin/apps/com.micropythonos.launcher/assets/launcher.py
+++ internal_filesystem/builtin/apps/com.micropythonos.launcher/assets/launcher.py
@@ -86,7 +86,8 @@ class Launcher(Activity):
         iconcont_height = icon_size + label_height
 
         for app in AppManager.get_app_list():
-            if app.category == "launcher":
+            if app.category == "launcher" or (app.fullname != "com.micropythonos.settings.wifi" and app.fullname.startswith("com.micropythonos.settings.")):
+                # Ignore launchers and MPOS settings (except wifi)
                 continue
 
             app_name = app.name
