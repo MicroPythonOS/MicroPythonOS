edeaa364e48e3c2bae9c606c5328754fe9c40db1
JensDiemer <git@jensdiemer.de>
2026-03-31 21:14:02 +0200

    Support Cheap Yellow Display
    see: https://github.com/MicroPythonOS/MicroPythonOS/discussions/88

--- internal_filesystem/lib/mpos/main.py
+++ internal_filesystem/lib/mpos/main.py
@@ -172,6 +172,10 @@ def detect_board():
         if unique_id_prefixes == b'\x30\xae\xa4':
             return "odroid_go"
 
+        print("cyd - Cheap Yellow Display ?")
+        if unique_id_prefixes == b'\xec\xe34': # FIXME: Use other detection! MAC seems to be random!
+            return "cyd"
+
         # Do I2C-based board detection
         # IMPORTANT: ESP32 GPIO 6-11 are internal SPI flash pins and will cause WDT reset if used.
         # ESP32-S3 has more usable GPIOs (up to 48). Detect chip variant first to skip unsafe probes.
