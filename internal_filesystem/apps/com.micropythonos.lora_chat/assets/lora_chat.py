9542c0ff96bbd1758ab36e5d6d1659174e1f460a
Thomas Farstrike <thomasverstreken@protonmail.com>
2026-04-01 19:27:17 +0200

    LoRa Chat: limit tweaks to fri3d_2026

--- internal_filesystem/apps/com.micropythonos.lora_chat/assets/lora_chat.py
+++ internal_filesystem/apps/com.micropythonos.lora_chat/assets/lora_chat.py
@@ -81,6 +81,11 @@ class LoRaChat(Activity):
         print(f"send result {result}: {SX1262.STATUS[result]}")
 
     def receive_callback(self, events):
+        print(f"receive_callback for events: {events}")
+        print(f"getRSSI: {self.lora_device.getRSSI()}")
+        print(f"getSNR: {self.lora_device.getSNR()}")
+        print(f"getStatus: {self.lora_device.getStatus()}")
+        print(f"getPacketStatus: {self.lora_device.getPacketStatus()}")
         if events & SX1262.TX_DONE:
             print('TX done.')
         elif events & SX1262.RX_DONE:
@@ -88,19 +93,14 @@ class LoRaChat(Activity):
             try:
                 print("self.lora_device.recv")
                 msg, err = self.lora_device.recv()
-                print("after self.lora_device.recv")
+                status = SX1262.STATUS[err]
+                print(f"after self.lora_device.recv, status: {status}")
                 if len(msg) > 0:
                     print(msg)
-                    self.alltext += "Received: " + msg.hex() + "\n"
+                    self.alltext += "Received: " + msg + "\n"
                     lv.async_call(lambda _: self.messages.set_text(self.alltext), None)
                 else:
                     print("len(msg) was 0")
-                status = SX1262.STATUS[err]
-                print(f"status: {status}")
-                print(f"getRSSI: {self.lora_device.getRSSI()}")
-                print(f"getSNR: {self.lora_device.getSNR()}")
-                print(f"getStatus: {self.lora_device.getStatus()}")
-                print(f"getPacketStatus: {self.lora_device.getPacketStatus()}")
             except Exception as e:
                 print(f"receive_thread got exception: {e}")
 
@@ -113,14 +113,20 @@ class LoRaChat(Activity):
             print("Not starting LoRa because simulation mode")
             return
 
-        rf_sw = Pin(46, Pin.OUT)
-        rf_sw.value(1) ; print("RF_SW set to HIGH") # Logic high level means enable receiver mode
+        # fri3d_2026 doesn't have a reset pin, instead it has RF_SW
+        from mpos import DeviceInfo
+        if DeviceInfo.hardware_id == "fri3d_2026":
+            rf_sw = Pin(46, Pin.OUT)
+            rf_sw.value(1) ; print("RF_SW set to HIGH") # Logic high level means enable receiver mode
 
         self.lora_device = mpos.sx
 
         self.lora_device.begin(freq=869.618, bw=62.5, sf=8, cr=8, syncWord=0x12, preambleLength=8, implicit=False, crcOn=True, tcxoVoltage=3.0, useRegulatorLDO=False, blocking=True)
         self.lora_device.setBlockingCallback(False, self.receive_callback)
-        self.lora_device.setDio2AsRfSwitch(False)
 
-        rf_sw.value(1) ; print("RF_SW set to HIGH")
+        if DeviceInfo.hardware_id == "fri3d_2026":
+            self.lora_device.setDio2AsRfSwitch(False)
+            rf_sw.value(1) ; print("RF_SW set to HIGH")
+
+        print("lora started")
 
