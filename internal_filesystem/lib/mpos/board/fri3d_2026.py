2deecc22959374315c5bd92329ac3ef17c7d35ab
Thomas Farstrike <thomasverstreken@protonmail.com>
2026-04-14 15:32:06 +0200

    Comments

--- internal_filesystem/lib/mpos/board/fri3d_2026.py
+++ internal_filesystem/lib/mpos/board/fri3d_2026.py
@@ -41,7 +41,7 @@ else:
     from drivers.lora.sx1262 import SX1262
     rf_sw = Pin(46, Pin.OUT)
     rf_sw.value(1) ; print("RF_SW set to HIGH") # Logic high level means enable receiver mode
-    sx = SX1262(lora_spi_device, 40, 11, 41, 45)
+    sx = SX1262(lora_spi_device, 40, 11, 41, 45) # reset pin isn't used but driver expects a value so set to 11 (IR receiver) here for now
     import mpos
     mpos.sx = sx
 
