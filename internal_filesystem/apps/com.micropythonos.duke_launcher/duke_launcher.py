715fa646f58332fdd599e6044bdfab29fe4693ad
Thomas Farstrike <thomasverstreken@protonmail.com>
2026-06-25 13:32:55 +0200

    Duke Launcher: add help text while starting

--- internal_filesystem/apps/com.micropythonos.duke_launcher/duke_launcher.py
+++ internal_filesystem/apps/com.micropythonos.duke_launcher/duke_launcher.py
@@ -5,6 +5,15 @@ from retrogo_launcher import RetroGoLauncher
 class DukeLauncher(Activity):
 
     def onCreate(self):
+        help_text = (
+            "- To change audio device and volume: MENU - GAME OPTIONS - RETROGO OPTIONS\n"
+            "- Hotkey mode: keep START pressed and then:\n"
+            "\t- look up/down: Y/B\n"
+            "\t- jetpack on: arrow down\n"
+            "\t- joystick left/right to scroll, arrow up to choose\n"
+            "- Shoot: A, Jump: B, Weapon: X, Crouch: Y, Use: START\n"
+            "- Autoaim is on so no no need to look up and down"
+        )
         self.startActivity(
             Intent(activity_class=RetroGoLauncher)
             .putExtra("title", "Choose your DUKE NUKEM 3D:")
@@ -13,4 +22,5 @@ class DukeLauncher(Activity):
             .putExtra("boot_name", "duke3d")
             .putExtra("game_name", "Duke Nukem 3D")
             .putExtra("file_extensions", (".grp", ".zip"))
+            .putExtra("starting_text", help_text)
         )
