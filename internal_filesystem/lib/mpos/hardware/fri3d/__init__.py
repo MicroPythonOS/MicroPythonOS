fc4cd38efca304c594df253858bf65f43a798e79
Thomas Farstrike <thomasverstreken@protonmail.com>
2026-06-28 22:59:13 +0200

    Remove unused files

--- internal_filesystem/lib/mpos/hardware/fri3d/__init__.py
+++
@@ -1,8 +0,0 @@
-# Fri3d Camp 2024 Badge Hardware Drivers
-# These are simple wrappers that can be used by services like AudioManager
-
-from .buzzer import BuzzerConfig
-from .leds import LEDConfig
-from .rtttl_data import RTTTL_SONGS
-
-__all__ = ['BuzzerConfig', 'LEDConfig', 'RTTTL_SONGS']
