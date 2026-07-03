d4c03b5667f7377cff4484df1715977ff14710ab
Thomas Farstrike <thomasverstreken@protonmail.com>
2026-07-03 17:49:48 +0200

    Try only disabling native, not viper

--- scripts/build_mpos.sh
+++ scripts/build_mpos.sh
@@ -5,9 +5,9 @@ mydir=$(dirname "$mydir")
 codebasedir=$(readlink -f "$mydir"/..) # build process needs absolute paths
 
 disable_native_viper() {
-	echo "NOT Disabling @micropython.native/@micropython.viper decorators for $target build..."
-	#find -L "$1" -name '*.py' -print0 | xargs -0 sed -i.bak -E 's/^([[:space:]]*)(@micropython\.(native|viper)[[:space:]]*)$/\1#\2/'
-	#find -L "$1" -name '*.py.bak' -delete
+	echo "Disabling @micropython.native decorators for $target build and leaving viper..."
+	find -L "$1" -name '*.py' -print0 | xargs -0 sed -i.bak -E 's/^([[:space:]]*)(@micropython\.(native)[[:space:]]*)$/\1#\2/'
+	find -L "$1" -name '*.py.bak' -delete
 }
 
 reset_web_port_changes() {
