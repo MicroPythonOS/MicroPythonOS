4c897e9ef321bfaaf3849902140239cd97548575
Thomas Farstrike <thomasverstreken@protonmail.com>
2026-07-15 19:06:50 +0200

    cleanup_pyc.sh: also .mpy (except breakout native app)

--- scripts/cleanup_pyc.sh
+++ scripts/cleanup_pyc.sh
@@ -1,2 +1,3 @@
 find internal_filesystem/ -iname "*.pyc" -exec rm {} \;
 find internal_filesystem/ -iname "__pycache__" -exec rmdir {} \;
+find internal_filesystem/ -iname "*.mpy" | grep -v breakout_ | while read file; do echo "deleting $file" ; rm "$file"; done
