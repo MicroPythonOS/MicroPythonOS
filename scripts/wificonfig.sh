2e7a2dc57d3bb8410278e406147f658b015b7f7a
Thomas Farstrike <thomasverstreken@protonmail.com>
2026-09-06 09:22:38 +0200

    Add scripts/wificonfig.sh

---
+++ scripts/wificonfig.sh
@@ -0,0 +1,6 @@
+mpremote.py mkdir :/prefs
+mpremote.py mkdir :/prefs/com.micropythonos.system.wifiservice
+#mpremote.py cp internal_filesystem/prefs/com.micropythonos.system.wifiservice/config.json_works :/prefs/com.micropythonos.system.wifiservice/config.json
+#mpremote.py cp internal_filesystem/data/prefs/com.micropythonos.system.wifiservice/config.json.orig :/prefs/com.micropythonos.system.wifiservice/config.json
+mpremote.py cp internal_filesystem/data/prefs/com.micropythonos.system.wifiservice/config.json :/prefs/com.micropythonos.system.wifiservice/config.json
+
